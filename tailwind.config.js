3c2baf716b4fd2c6971251c78c1bdec9a6350f2a
Eduardo <ejrb1234@gmail.com>
2024-10-21 15:59:02 -0400

    build(app): Using new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        'cerise': {
+        50: '#fdf3f4',
+        100: '#fbe8e9',
+        200: '#f8d3d8',
+        300: '#f1b0b9',
+        400: '#e98393',
+        500: '#dd566e',
+        600: '#c9395a',
+        700: '#a82848',
+        800: '#8d2442',
+        900: '#79223e',
+        950: '#430e1e',
+    },
       },
     },
   },
   plugins: [],
 }
+
+
