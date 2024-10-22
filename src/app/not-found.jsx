a7b173bef4d1c3c1bd19195dc472c059e84567c9
Eduardo <ejrb1234@gmail.com>
2024-10-22 18:14:24 -0400

    NotFound #404 (Page)

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,17 @@
+
+import { Button } from "@/app/MTailwind"
+import Link from "next/link"
+
 export const metadata = {
   title: 'Not found page',
 }
 
 export default function NotFound() {
   return (
-    <div>
-      <p>#404</p>
-    </div>
+    <Link href="/login">
+      <Button variant="outlined">
+        Volver a la página principal
+      </Button>
+    </Link>
   )
 }
