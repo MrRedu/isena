2593bd034b4be18dd9be9edcacc4ea89e8c74c10
Eduardo <ejrb1234@gmail.com>
2024-10-22 18:14:04 -0400

    Toaster (Sonner package) + Session Provider = ProviderComponent

---
+++ src/app/components/Providers.jsx
@@ -0,0 +1,16 @@
+'use client'
+import propTypes from 'prop-types'
+import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'sonner'
+export function Providers({ children, session }) {
+  return (
+    <>
+      <Toaster />
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </>
+  )
+}
+Providers.propTypes = {
+  children: propTypes.node,
+  session: propTypes.object,
+}
