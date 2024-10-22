7e6fbb0037f4be2ea6f049ba87498eb7dae48017
Eduardo <ejrb1234@gmail.com>
2024-10-22 18:13:35 -0400

    MySQL + NextAuth are here! 🐐

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,71 @@
+import CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth/next'
+import { validatePassword } from '@/services/authServices'
+
+async function login(credentials) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/users/${credentials.email}`
+    )
+    if (!response.ok) throw new Error('Something went wrong')
+    const { data: user } = await response.json()
+    if (!user) throw new Error('Incorrect credentials')
+
+    const isPasswordValid = await validatePassword(
+      credentials.password,
+      user.contrasena_usuario
+    )
+
+    if (!isPasswordValid) throw new Error('Incorrect credentials')
+    return user
+  } catch (error) {
+    console.error('Error while logging in: ', error)
+    throw new Error('Something went wrong')
+  }
+}
+
+export const authOptions = {
+  pages: {
+    signIn: '/',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {},
+      authorize: async credentials => {
+        try {
+          const user = await login(credentials)
+          return user
+        } catch (error) {
+          throw new Error('Failed to login')
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id_user = user.id_usuario
+        token.email = user.correo_usuario
+        token.name = user.nombres_usuario
+        token.id_rol = user.id_rol_usuario
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id_user = token.id_user
+        session.user.email = token.email
+        session.user.name = token.name
+        session.user.id_rol = token.id_rol
+      }
+      return session
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
---
+++ src/app/utils/consts.js
@@ -0,0 +1,4 @@
+export const initialValueFormLogin = {
+  email: '',
+  password: '',
+}
