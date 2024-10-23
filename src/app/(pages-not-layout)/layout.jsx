7e64c954f562f900eac8c687a539c70293fb1b4a
Eduardo <ejrb1234@gmail.com>
2024-10-23 18:27:26 -0400

    Header, Sidebar, Section

--- src/app/(pages-not-layout)/layout.jsx
+++ src/app/(pages-not-layout)/layout.jsx
@@ -1,14 +1,12 @@
-'use client'
 import propTypes from 'prop-types'
-import { SessionRedirect } from '@/components/SessionRedirec'
 
 export default function Layout({ children }) {
   return (
-    <SessionRedirect>
+    <>
       <h2>Header</h2>
       {children}
       <footer>Footer</footer>
-    </SessionRedirect>
+    </>
   )
 }
 
--- src/app/components/Providers.jsx
+++ src/app/components/Providers.jsx
@@ -2,11 +2,16 @@
 import propTypes from 'prop-types'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'sonner'
+import { SessionRedirect } from './SessionRedirec'
 export function Providers({ children, session }) {
   return (
     <>
       <Toaster />
-      <SessionProvider session={session}>{children}</SessionProvider>
+      <SessionProvider session={session}>
+        <SessionRedirect>
+          {children}
+        </SessionRedirect>
+      </SessionProvider>
     </>
   )
 }
