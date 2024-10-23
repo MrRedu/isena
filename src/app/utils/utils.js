27cd7117766ba2ffcd004e9b0c3399e0f5cfa0ff
Eduardo <ejrb1234@gmail.com>
2024-10-23 18:26:48 -0400

    Utils

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -3,3 +3,18 @@ export const validateEmail = email => {
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return emailRegex.test(String(email).toLowerCase())
 }
+
+export function whatIsMyAge(fechaNacimiento) {
+  let fechaNace = new Date(fechaNacimiento);
+  let fechaActual = new Date()
+
+  let mes = fechaActual.getMonth();
+  let dia = fechaActual.getDate();
+  let año = fechaActual.getFullYear();
+
+  fechaActual.setDate(dia);
+  fechaActual.setMonth(mes);
+  fechaActual.setFullYear(año);
+
+    return Math.floor(((fechaActual - fechaNace) / (1000 * 60 * 60 * 24) / 365));;
+}
