e61f8628d485e190b8c34660df7b0c9b56107cd6
Eduardo <ejrb1234@gmail.com>
2024-10-24 18:50:40 -0400

    FormatDate function (utils)

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -16,5 +16,31 @@ export function whatIsMyAge(fechaNacimiento) {
   fechaActual.setMonth(mes);
   fechaActual.setFullYear(año);
 
-    return Math.floor(((fechaActual - fechaNace) / (1000 * 60 * 60 * 24) / 365));;
+  return Math.floor(((fechaActual - fechaNace) / (1000 * 60 * 60 * 24) / 365));;
+}
+
+export function formatDate(isoDate) {
+  const date = new Date(isoDate);
+  
+  // Obtener el día, mes y año
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const monthNames = [
+      "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+      "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+  const month = monthNames[date.getUTCMonth()]; // Mes comienza en 0
+  const year = date.getUTCFullYear();
+  
+  // Formatear la fecha
+  return `${day} ${month.slice(0, 3)}. ${year}`;
+}
+
+export function formatNumber(number = 0) {
+  // Convertir el número a cadena
+  let numberStr = number.toString();
+  
+  // Usar una expresión regular para insertar puntos
+  let resultado = numberStr.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  
+  return resultado;
 }
