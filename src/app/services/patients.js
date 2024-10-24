72a70d2bedb0f4ce96c943d63c0cd45062eee7a3
Eduardo <ejrb1234@gmail.com>
2024-10-24 18:49:13 -0400

    {Patients} data get from database

--- src/app/services/patients.js
+++ src/app/services/patients.js
@@ -1,5 +1,23 @@
+export const getAllPatients = async () => {
+  const result = await fetch(`http://localhost:3000/api/patients`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    cache: 'no-cache'
+  })
+  const patients = await result.json()
+  return patients
+}
+
 export const getPatient = async ({ cedula }) => {
-  const result = await fetch(`http://localhost:3000/api/patients/${cedula}`)
+  const result = await fetch(`http://localhost:3000/api/patients/${cedula}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    cache: 'no-cache'
+  })
   const patient = await result.json()
   return patient
 }
