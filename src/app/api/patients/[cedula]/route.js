8c1121ad52ad429ce9fe776966c618017a0680b2
Eduardo <ejrb1234@gmail.com>
2024-10-24 18:49:55 -0400

    Update query to get patients data from database

--- src/app/api/patients/[cedula]/route.js
+++ src/app/api/patients/[cedula]/route.js
@@ -4,7 +4,64 @@ import { connection } from '@/libs/mysql'
 export async function GET(req, { params }) {
   try {
     const [result] = await connection.query(
-      'SELECT * FROM tbl_pacientes WHERE cedula_paciente = ?',
+      `
+SELECT 
+    p.id_paciente,
+    p.nombres_paciente,
+    p.apellidos_paciente,
+    p.cedula_paciente,
+    p.telefono_paciente,
+    p.fecha_nacimiento_paciente,
+
+    -- Agrupando pesos en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_peso', pw.fecha_registro, 'valor', pw.peso))
+     FROM tbl_pesos pw 
+     WHERE pw.id_paciente = p.id_paciente) AS pesos,
+
+    -- Agrupando alturas en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_altura', pa.fecha_registro, 'valor', pa.altura))
+     FROM tbl_alturas pa 
+     WHERE pa.id_paciente = p.id_paciente) AS alturas,
+
+    -- Agrupando temperaturas en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_temperatura', pt.fecha_registro, 'valor', pt.temperatura))
+     FROM tbl_temperaturas pt 
+     WHERE pt.id_paciente = p.id_paciente) AS temperaturas,
+
+    -- Agrupando frecuencias respiratorias en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_frecuencia', fr.fecha_registro, 'valor', fr.frecuencia_respiratoria))
+     FROM tbl_frecuencias_respiratorias fr 
+     WHERE fr.id_paciente = p.id_paciente) AS frecuencias_respiratorias,
+
+    -- Agrupando presiones arteriales en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_presion', pr.fecha_registro, 'sistolica', pr.presion_sistolica, 'diastolica', pr.presion_diastolica))
+     FROM tbl_presiones_arteriales pr 
+     WHERE pr.id_paciente = p.id_paciente) AS presiones_arteriales,
+
+    -- Agrupando frecuencias cardíacas en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT('fecha_frecuencia_cardiaca', fc.fecha_registro, 'valor', fc.frecuencia_cardiaca))
+     FROM tbl_frecuencias_cardiacas fc 
+     WHERE fc.id_paciente = p.id_paciente) AS frecuencias_cardiacas,
+
+    -- Agrupando medicamentos en un array
+    (SELECT JSON_ARRAYAGG(JSON_OBJECT(
+        'id_medicamento', m.id_medicamento,
+        'nombre_medicamento', m.nombre_medicamento,
+        'dosis_medicamento', m.dosis_medicamento,
+        'via_administracion_medicamento', m.via_administracion_medicamento,
+        'intervalo_medicamento', m.intervalo_medicamento,
+        'fecha_inicio_medicamento', m.fecha_inicio_medicamento,
+        'fecha_fin_medicamento', m.fecha_fin_medicamento
+      ))
+     FROM tbl_medicamentos m 
+     WHERE m.id_paciente = p.id_paciente) AS medicamentos
+
+FROM 
+    tbl_pacientes p
+
+WHERE 
+    p.cedula_paciente = ?
+      `,
       [params.cedula]
     )
 
