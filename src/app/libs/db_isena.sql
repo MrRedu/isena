742f792bc91250ea3d8f91887330d0f6d78cc2ec
Eduardo <ejrb1234@gmail.com>
2024-11-01 18:58:01 -0400

    Updated DB

--- src/app/libs/db_isena.sql
+++ src/app/libs/db_isena.sql
@@ -46,10 +46,9 @@ CREATE TABLE `tbl_bitacora` (
 
 DROP TABLE IF EXISTS `tbl_pacientes`;
 CREATE TABLE `tbl_pacientes` (
-  `id_paciente` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Apellidos del paciente',
+  `cedula_paciente` int(11) NOT NULL PRIMARY KEY COMMENT 'Cédula de identidad del paciente',
   `nombres_paciente` varchar(64) NOT NULL COMMENT 'Nombres del paciente',
   `apellidos_paciente` varchar(64) NOT NULL COMMENT 'Apellidos del paciente',
-  `cedula_paciente` varchar(8) NOT NULL COMMENT 'Cédula de identidad del paciente',
   `telefono_paciente` varchar(11) NOT NULL COMMENT 'Teléfono del paciente',
   `fecha_nacimiento_paciente` DATE NOT NULL COMMENT 'Fecha de nacimiento del paciente',
   `correo_paciente` varchar(82) COMMENT 'Correo del paciente',
@@ -63,92 +62,93 @@ CREATE TABLE `tbl_pacientes` (
   -- frecuencias cardíacas
   -- medicamentos
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de pacientes';
-INSERT INTO `tbl_pacientes` (`id_paciente`, `nombres_paciente`, `apellidos_paciente`, `cedula_paciente`, `telefono_paciente`, `fecha_nacimiento_paciente`, `correo_paciente`, `direccion_paciente`) VALUES (1, 'Luis Andrés', 'Chen Romero', '12345678', '04141234567', '2000-01-01', '7Dj5V@example.com', 'Calle 1 # 2-3'), (2, 'José Ramón', 'Fernandez Gutierrez', '87654321', '04147654321', '1980-06-06', '7Dj5V@example.com', 'Calle 2 # 3-4');
+INSERT INTO `tbl_pacientes` (`cedula_paciente`, `nombres_paciente`, `apellidos_paciente`, `telefono_paciente`, `fecha_nacimiento_paciente`, `correo_paciente`, `direccion_paciente`) VALUES (12345678, 'Luis Andrés', 'Cheng Romero', '04141234567', '2000-01-01', '7Dj5V@example.com', 'Calle 1 # 2-3'), (87654321, 'José Ramón', 'Fernandez Gutierrez', '04147654321', '1980-06-06', '7Dj5V@example.com', 'Calle 2 # 3-4');
 
 DROP TABLE IF EXISTS `tbl_alturas`;
 CREATE TABLE `tbl_alturas` (
   `id_altura` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID del registro de altura',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
   `altura` decimal(5,2) NOT NULL COMMENT 'Altura del paciente registrado',
   `fecha_registro` DATE NOT NULL  COMMENT 'Fecha del registro en la tabla de alturas',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de alturas por paciente';
-INSERT INTO `tbl_alturas` (`id_altura`, `id_paciente`, `altura`, `fecha_registro`) VALUES (1, 1, '1.75', '2022-01-01'), (2, 1, '1.80', '2022-06-06'), (3, 1, '1.90', '2023-01-01'), (4, 1, '1.95', '2023-06-06'), (5, 2, '1.75', '2022-01-01'), (6, 2, '1.80', '2022-06-06'), (7, 2, '1.90', '2023-01-01'), (8, 2, '1.95', '2023-06-06');
+INSERT INTO `tbl_alturas` (`id_altura`, `cedula_paciente`, `altura`, `fecha_registro`) VALUES (1, 12345678, '1.75', '2022-01-01'), (2, 12345678, '1.80', '2022-06-06'), (3, 12345678, '1.90', '2023-01-01'), (4, 12345678, '1.95', '2023-06-06'), (5, 87654321, '1.75', '2022-01-01'), (6, 87654321, '1.80', '2022-06-06'), (7, 87654321, '1.90', '2023-01-01'), (8, 87654321, '1.95', '2023-06-06');
 
 DROP TABLE IF EXISTS `tbl_pesos`;
 CREATE TABLE `tbl_pesos` (
   `id_peso` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID del registro de peso',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
   `peso` decimal(5,2) NOT NULL COMMENT 'Peso del paciente registrado',
   `fecha_registro` DATE NOT NULL COMMENT 'Fecha del registro en la tabla de pesos',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de pesos por paciente';
-INSERT INTO `tbl_pesos` (`id_peso`, `id_paciente`, `peso`, `fecha_registro`) VALUES (1, 1, '75.00', '2022-01-01'), (2, 1, '80.00', '2022-06-06'), (3, 1, '90.00', '2023-01-01'), (4, 1, '95.00', '2023-06-06'), (5, 2, '75.00', '2022-01-01'), (6, 2, '80.00', '2022-06-06'), (7, 2, '90.00', '2023-01-01'), (8, 2, '95.00', '2023-06-06');
+INSERT INTO `tbl_pesos` (`id_peso`, `cedula_paciente`, `peso`, `fecha_registro`) VALUES (1, 12345678, '75.00', '2022-01-01'), (2, 12345678, '80.00', '2022-06-06'), (3, 12345678, '90.00', '2023-01-01'), (4, 12345678, '95.00', '2023-06-06'), (5, 87654321, '75.00', '2022-01-01'), (6, 87654321, '80.00', '2022-06-06'), (7, 87654321, '90.00', '2023-01-01'), (8, 87654321, '95.00', '2023-06-06');
 
 DROP TABLE IF EXISTS `tbl_temperaturas`;
 CREATE TABLE `tbl_temperaturas` (
   `id_temperatura` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID del registro de temperatura',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al cual pertenece el peso',
   `temperatura` decimal(4,2) NOT NULL COMMENT 'Temperatura del paciente registrado',
   `fecha_registro` DATE NOT NULL COMMENT 'Fecha del registro en la tabla de temperaturas',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de temperaturas por paciente';
-INSERT INTO `tbl_temperaturas` (`id_temperatura`, `id_paciente`, `temperatura`, `fecha_registro`) VALUES (1, 1, '36.00', '2022-01-01'), (2, 1, '37.00', '2022-06-06'), (3, 1, '38.00', '2023-01-01'), (4, 1, '39.00', '2023-06-06'), (5, 2, '36.00', '2022-01-01'), (6, 2, '37.00', '2022-06-06'), (7, 2, '38.00', '2023-01-01'), (8, 2, '39.00', '2023-06-06');
+INSERT INTO `tbl_temperaturas` (`id_temperatura`, `cedula_paciente`, `temperatura`, `fecha_registro`) VALUES (1, 12345678, '36.00', '2022-01-01'), (2, 12345678, '37.00', '2022-06-06'), (3, 12345678, '38.00', '2023-01-01'), (4, 12345678, '39.00', '2023-06-06'), (5, 87654321, '36.00', '2022-01-01'), (6, 87654321, '37.00', '2022-06-06'), (7, 87654321, '38.00', '2023-01-01'), (8, 87654321, '39.00', '2023-06-06');
 
 DROP TABLE IF EXISTS `tbl_frecuencias_respiratorias`;
 CREATE TABLE `tbl_frecuencias_respiratorias` (
   `id_frecuencia` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID de la frecuencia respiratoria',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la frecuencia respiratoria', 
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la frecuencia respiratoria', 
   `frecuencia_respiratoria` int(3) NOT NULL COMMENT 'Frecuencia respiratoria del paciente',
   `fecha_registro` DATE NOT NULL COMMENT 'Fecha en que se realizó el registro',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de frecuencias respiratorias por paciente';
-INSERT INTO `tbl_frecuencias_respiratorias` (`id_frecuencia`, `id_paciente`, `frecuencia_respiratoria`, `fecha_registro`) VALUES (1, 1, 80, '2022-01-01'), (2, 1, '90', '2022-06-06'), (3, 1, 9, '2023-01-01'), (4, 1, 120, '2023-06-06'), (5, 2, 80, '2022-01-01'), (6, 2, 90, '2022-06-06'), (7, 2, 9, '2023-01-01'), (8, 2, 120, '2023-06-06');
+INSERT INTO `tbl_frecuencias_respiratorias` (`id_frecuencia`, `cedula_paciente`, `frecuencia_respiratoria`, `fecha_registro`) VALUES (1, 12345678, 80, '2022-01-01'), (2, 12345678, '90', '2022-06-06'), (3, 12345678, 9, '2023-01-01'), (4, 12345678, 120, '2023-06-06'), (5, 87654321, 80, '2022-01-01'), (6, 87654321, 90, '2022-06-06'), (7, 87654321, 9, '2023-01-01'), (8, 87654321, 120, '2023-06-06');
 
 DROP TABLE IF EXISTS `tbl_presiones_arteriales`;
 CREATE TABLE `tbl_presiones_arteriales` (
   `id_presion` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID de la presión arterial',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la presión arterial',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la presión arterial',
   `presion_sistolica` int(3) NOT NULL COMMENT 'Presión sistólica',
   `presion_diastolica` int(3) NOT NULL COMMENT 'Presión distólica',
   `fecha_registro` DATE NOT NULL COMMENT 'Fecha en que se realizó el registro',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de presiones arteriales por paciente';
-INSERT INTO `tbl_presiones_arteriales` (`id_presion`, `id_paciente`, `presion_sistolica`, `presion_diastolica`, `fecha_registro`) VALUES (1, 1, 120, 80, '2022-01-01'), (2, 1, 130, 90, '2022-06-06'), (3, 1, 140, 100, '2023-01-01'), (4, 1, 150, 110, '2023-06-06'), (5, 2, 120, 80, '2022-01-01'), (6, 2, 130, 90, '2022-06-06'), (7, 2, 140, 100, '2023-01-01'), (8, 2, 150, 110, '2023-06-06');
+INSERT INTO `tbl_presiones_arteriales` (`id_presion`, `cedula_paciente`, `presion_sistolica`, `presion_diastolica`, `fecha_registro`) VALUES (1, 12345678, 120, 80, '2022-01-01'), (2, 12345678, 130, 90, '2022-06-06'), (3, 12345678, 140, 100, '2023-01-01'), (4, 12345678, 150, 110, '2023-06-06'), (5, 87654321, 120, 80, '2022-01-01'), (6, 87654321, 130, 90, '2022-06-06'), (7, 87654321, 140, 100, '2023-01-01'), (8, 87654321, 150, 110, '2023-06-06');
 
 DROP DATABASE IF EXISTS `tbl_frecuencias_cardiacas`;
 CREATE TABLE `tbl_frecuencias_cardiacas` (
   `id_frecuencia` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID de la frecuencia cardiaca',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la frecuencia cardiaca',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la frecuencia cardiaca',
   `frecuencia_cardiaca` int(3) NOT NULL COMMENT 'Frecuencia cardiaca del paciente',
   `fecha_registro` DATE NOT NULL COMMENT 'Fecha en que se realizó el registro',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de frecuencias cardiacas por paciente';
-INSERT INTO `tbl_frecuencias_cardiacas` (`id_frecuencia`, `id_paciente`, `frecuencia_cardiaca`, `fecha_registro`) VALUES (1, 1, 80, '2022-01-01'), (2, 1, 90, '2022-06-06'), (3, 1, 100, '2023-01-01'), (4, 1, 110, '2023-06-06'), (5, 2, 80, '2022-01-01'), (6, 2, 90, '2022-06-06'), (7, 2, 100, '2023-01-01'), (8, 2, 110, '2023-06-06');
+INSERT INTO `tbl_frecuencias_cardiacas` (`id_frecuencia`, `cedula_paciente`, `frecuencia_cardiaca`, `fecha_registro`) VALUES (1, 12345678, 80, '2022-01-01'), (2, 12345678, 90, '2022-06-06'), (3, 12345678, 100, '2023-01-01'), (4, 12345678, 110, '2023-06-06'), (5, 87654321, 80, '2022-01-01'), (6, 87654321, 90, '2022-06-06'), (7, 87654321, 100, '2023-01-01'), (8, 87654321, 110, '2023-06-06');
 
 DROP TABLE IF EXISTS `tbl_medicamentos`;
 CREATE TABLE `tbl_medicamentos` (
   `id_medicamento` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID del medicamento',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde el medicamento',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'Cedula del paciente',
   `nombre_medicamento` varchar(128) NOT NULL COMMENT 'Nombre del medicamento',
   `dosis_medicamento` varchar(64) NOT NULL COMMENT 'Dosis/explicación del medicamento',
   `via_administracion_medicamento` varchar(64) NOT NULL COMMENT 'Via de administración del medicamento',
   `intervalo_medicamento` varchar(64) NOT NULL COMMENT 'Duración del medicamento',
   `fecha_inicio_medicamento` DATE NOT NULL COMMENT 'Fecha de inicio para el medicamento',
   `fecha_fin_medicamento` DATE COMMENT 'Fecha de fin para el medicamento',
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`)
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de medicamentos';
-INSERT INTO `tbl_medicamentos` (`id_paciente`, `nombre_medicamento`, `dosis_medicamento`, `via_administracion_medicamento`, `intervalo_medicamento`, `fecha_inicio_medicamento`, `fecha_fin_medicamento`) VALUES (1, 'Paracetamol', '500 mg', 'Oral', 'Cada 8 horas', '2024-10-01', '2024-10-06'), (1, 'Ibuprofeno', '400 mg', 'Oral', 'Cada 6 horas', '2024-10-05', NULL), (1, 'Amoxicilina', '250 mg', 'Oral', 'Cada 12 horas', '2024-10-10', '2024-10-20'), (1, 'Loratadina', '10 mg', 'Oral', 'Una vez al día', '2024-10-15', NULL), (1, 'Metformina', '500 mg', 'Oral', 'Cada 12 horas', '2024-09-01', NULL), (1, 'Salbutamol', '100 mcg', 'Inhalación', 'Según necesidad', '2024-10-01', NULL), (1, 'Losartán', '50 mg', 'Oral', 'Una vez al día', '2024-08-15', NULL), (1, 'Omeprazol', '20 mg', 'Oral', 'Una vez al día antes de comer', '2024-09-10', NULL), (2, 'Paracetamol', '500 mg', 'Oral', 'Cada 8 horas', '2024-10-01', '2024-10-06'), (2, 'Ibuprofeno', '400 mg', 'Oral', 'Cada 6 horas', '2024-10-05', NULL), (2, 'Amoxicilina', '250 mg', 'Oral', 'Cada 12 horas', '2024-10-10', '2024-10-20'), (2, 'Loratadina', '10 mg', 'Oral', 'Una vez al día', '2024-10-15', NULL), (2, 'Metformina', '500 mg', 'Oral', 'Cada 12 horas', '2024-09-01', NULL), (2, 'Salbutamol', '100 mcg', 'Inhalación', 'Según necesidad', '2024-10-01', NULL), (2, 'Losartán', '50 mg', 'Oral', 'Una vez al día', '2024-08-15', NULL);
+INSERT INTO `tbl_medicamentos` (`cedula_paciente`, `nombre_medicamento`, `dosis_medicamento`, `via_administracion_medicamento`, `intervalo_medicamento`, `fecha_inicio_medicamento`, `fecha_fin_medicamento`) VALUES (12345678, 'Paracetamol', '500 mg', 'Oral', 'Cada 8 horas', '2024-10-01', '2024-10-06'), (12345678, 'Ibuprofeno', '400 mg', 'Oral', 'Cada 6 horas', '2024-10-05', NULL), (12345678, 'Amoxicilina', '250 mg', 'Oral', 'Cada 12 horas', '2024-10-10', '2024-10-20'), (12345678, 'Loratadina', '10 mg', 'Oral', 'Una vez al día', '2024-10-15', NULL), (12345678, 'Metformina', '500 mg', 'Oral', 'Cada 12 horas', '2024-09-01', NULL), (12345678, 'Salbutamol', '100 mcg', 'Inhalación', 'Según necesidad', '2024-10-01', NULL), (12345678, 'Losartán', '50 mg', 'Oral', 'Una vez al día', '2024-08-15', NULL), (12345678, 'Omeprazol', '20 mg', 'Oral', 'Una vez al día antes de comer', '2024-09-10', NULL), (87654321, 'Paracetamol', '500 mg', 'Oral', 'Cada 8 horas', '2024-10-01', '2024-10-06'), (87654321, 'Ibuprofeno', '400 mg', 'Oral', 'Cada 6 horas', '2024-10-05', NULL), (87654321, 'Amoxicilina', '250 mg', 'Oral', 'Cada 12 horas', '2024-10-10', '2024-10-20'), (87654321, 'Loratadina', '10 mg', 'Oral', 'Una vez al día', '2024-10-15', NULL), (87654321, 'Metformina', '500 mg', 'Oral', 'Cada 12 horas', '2024-09-01', NULL), (87654321, 'Salbutamol', '100 mcg', 'Inhalación', 'Según necesidad', '2024-10-01', NULL), (87654321, 'Losartán', '50 mg', 'Oral', 'Una vez al día', '2024-08-15', NULL);
 
+DROP TABLE IF EXISTS `tbl_consultas`;
 CREATE TABLE `tbl_consultas` (
   `id_consulta` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID de la consulta',
-  `id_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la consulta',
+  `cedula_paciente` int(11) NOT NULL COMMENT 'ID del paciente al que le corresponde la consulta',
   `id_usuario` int(11) NOT NULL COMMENT 'ID del usuario que registró la consulta',
   `fecha_consulta` datetime NOT NULL COMMENT 'Fecha y hora para la consulta',
   `motivo_consulta` varchar(255) NOT NULL COMMENT 'Motivo/descripción de la consulta',
   `estado_consulta` varchar(255) NOT NULL COMMENT 'Estado de la consulta',
 
-  FOREIGN KEY (`id_paciente`) REFERENCES `tbl_pacientes`(`id_paciente`),
+  FOREIGN KEY (`cedula_paciente`) REFERENCES `tbl_pacientes`(`cedula_paciente`),
   FOREIGN KEY (`id_usuario`) REFERENCES `tbl_usuarios`(`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabla de consultas';
 
