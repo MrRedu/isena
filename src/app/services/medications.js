02eb9f5831ec5b12a3bd4b2324f184f32682a6ef
Eduardo <ejrb1234@gmail.com>
2024-11-01 18:58:23 -0400

    Services: getMedicationsByCedula

--- src/app/services/medications.js
+++ src/app/services/medications.js
@@ -1,5 +1,22 @@
 import { toast } from "sonner"
 
+export const getMedicationsByCedula = async ({cedulaPaciente} , {signal}) => {
+  const URL = `http://localhost:3000/api/patients/${cedulaPaciente}/medications`
+  try {
+    const result = await fetch(URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      signal
+    })
+    const {data} = await result.json()
+    return data
+  } catch (error) {
+    throw new Error('Error loading medications')
+  }
+}
+
 export const deleteMedication = async id => {
   const result = await fetch(`http://localhost:3000/api/medications/${id}`, {
     method: 'DELETE',
@@ -7,11 +24,6 @@ export const deleteMedication = async id => {
       'Content-Type': 'application/json',
     },
   })
-
-  if (!result.ok) {
-    return toast.error('Error al eliminar el medicamento')
-  }
-  if (result.ok) {
-    return toast.success(`El medicamento ha sido eliminado`)
-  }
+  if (!result.ok) return toast.error('Error al eliminar el medicamento')
+  if (result.ok) return toast.success(`El medicamento ha sido eliminado`)
 }
