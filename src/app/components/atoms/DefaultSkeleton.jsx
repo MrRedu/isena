a1dfcf2fbe03d3ada9833df5e94f07284c78f4fa
Eduardo <ejrb1234@gmail.com>
2024-11-04 16:06:01 -0400

    Default skeleton component

---
+++ src/app/components/atoms/DefaultSkeleton.jsx
@@ -0,0 +1,42 @@
+import { Typography } from "@/app/MTailwind";
+export function DefaultSkeleton() {
+  return (
+    <div className="max-w-full animate-pulse">
+      <Typography
+        as="div"
+        variant="h2"
+        className="mb-4 h-3 w-56 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+      <Typography
+        as="div"
+        variant="paragraph"
+        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
+      >
+        &nbsp;
+      </Typography>
+    </div>
+  );
+}
--- src/app/components/organisms/PatientProfile.jsx
+++ src/app/components/organisms/PatientProfile.jsx
@@ -4,15 +4,27 @@ import { Card } from "@/app/MTailwind";
 import { usePatient } from '@/hooks/usePatients';
 import { format } from '@formkit/tempo';
 import { formatNumber } from '@/utils/utils';
+import { DefaultSkeleton } from '@/components/atoms/DefaultSkeleton';
 export const PatientProfile = ({ cedulaPaciente }) => {
   const { patient, isLoading } = usePatient({ cedulaPaciente })
+  console.log(patient)
   return (
     <Card className='rounded-none border shadow-none overflow-hidden p-4'>
-      <h2 className='font-bold uppercase text-lg flex flex-col'>
-        <span>{patient?.nombres || 'Nombres'}</span>
-        <span>{patient?.apellidos || 'Apellidos'}</span>
-      </h2>
-      <p className=''>{formatNumber(patient?.cedula) || 'Cédula'} / {format(patient?.fechaNacimiento, "MMM D, YYYY", "es") || 'Fecha de nacimiento'}</p>
+      {isLoading && <DefaultSkeleton />}
+      {!isLoading &&
+        <>
+          <h2 className='font-bold uppercase text-lg flex flex-col'>
+            <span>{patient?.nombres || 'Nombres'}</span>
+            <span>{patient?.apellidos || 'Apellidos'}</span>
+          </h2>
+          <p className=''>
+            {formatNumber(patient?.cedula) || 'Cédula'} / {format(patient?.fechaNacimiento, "MMM D, YYYY", "es") || 'Fecha de nacimiento'}
+          </p>
+          <p>
+            {patient?.telefono || 'Teléfono'} / {patient?.email || 'Correo eléctronico'}
+          </p>
+        </>
+      }
     </Card>
   )
 };
