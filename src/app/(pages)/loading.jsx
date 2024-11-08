9f7618a846d45c9035c162303228d2dc2a50aa5b
Eduardo <ejrb1234@gmail.com>
2024-11-08 01:43:44 -0400

    New spinner loader

--- src/app/(pages)/loading.jsx
+++ src/app/(pages)/loading.jsx
@@ -1,7 +1,13 @@
 export default function PageLoading() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+    <div className="flex justify-center items-center h-[calc(100vh-92px)]">
+      <div
+        className="w-20 h-20 border-4 border-transparent text-blush-700 text-4xl animate-spin flex items-center justify-center border-t-blush-700 rounded-full"
+      >
+        <div
+          className="w-16 h-16 border-4 border-transparent text-blush-400 text-2xl animate-spin flex items-center justify-center border-t-blush-400 rounded-full"
+        ></div>
+      </div>
     </div>
   )
 }
