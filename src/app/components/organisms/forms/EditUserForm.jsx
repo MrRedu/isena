7befa4c6a9b91f2a51a76bdf2dfd7f8fb2e4afad
Eduardo <ejrb1234@gmail.com>
2024-11-11 18:17:23 -0400

    EditUserForm new styles! 🐐

--- src/app/components/organisms/forms/EditUserForm.jsx
+++ src/app/components/organisms/forms/EditUserForm.jsx
@@ -1,16 +1,28 @@
 'use client'
-import { Option, Select, Switch } from "@/app/MTailwind";
+import { Input, Option, Select, Switch, Typography } from "@/app/MTailwind";
+import { useState } from "react";
 
 export const EditUserForm = () => {
+  const [status, setStatus] = useState(true)
+  const handleChange = () => setStatus(!status)
+
   return (
     <form className="flex flex-col gap-8 p-4">
-      <Select variant="static" label="Seleccionar rol">
+      <div className="flex gap-8 flex-col lg:flex-row">
+        <Input type="text" variant="static" label="Nombres" readOnly />
+        <Input type="text" variant="static" label="Apellidos" readOnly />
+      </div>
+      <Input type="text" variant="static" label="Correo electrónico" readOnly />
+      <Select variant="static" label="Seleccionar rol" >
         <Option>Administrador</Option>
         <Option>Desarrollador</Option>
         <Option>Médico</Option>
         <Option>Visualizador</Option>
       </Select>
-      <Switch label="Habilitar" />
+      <div className="flex flex-col gap-4">
+        <Typography className="text-sm">Status</Typography>
+        <Switch checked={status} onChange={handleChange} label={status ? "Deshabilitar" : "Habilitar"} />
+      </div>
     </form>
   )
 };
