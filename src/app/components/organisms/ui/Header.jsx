ef77c80ec6c977758a9e64cdab28b18abb16b2bf
Eduardo <ejrb1234@gmail.com>
2024-11-17 11:26:12 -0400

    visual bug solved

--- src/app/components/organisms/ui/Header.jsx
+++ src/app/components/organisms/ui/Header.jsx
@@ -4,7 +4,7 @@ import { Avatar } from '@/components/organisms/ui/Avatar'
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between px-4 py-2 sticky z-10 top-0 shadow ">
+    <header className="flex items-center bg-white justify-between px-4 py-2 sticky z-10 top-0 shadow ">
       <SidebarWithBurgerMenu />
       <Avatar />
     </header>
