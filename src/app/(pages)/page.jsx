a44a165bb7802a081ac2f8eb64f15da9d30c7a2a
Eduardo <ejrb1234@gmail.com>
2024-12-04 16:08:43 -0400

    Dashboard is working now.

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,59 +1,35 @@
-import Link from 'next/link'
 import { Section } from '@/components/atoms/Section'
-import { Button, Card, CardBody, CardFooter, Typography } from '@/app/MTailwind'
-import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import { UserIcon } from '@heroicons/react/24/outline'
+import { CardDashboard } from '@/components/molecules/CardDashboard'
+import { numberOfUsers } from '@/services/users'
+import { numberOfPatients } from '@/services/patients'
 
 export const metadata = {
   title: 'Panel de control',
   description: 'Página del panel de control',
 }
 
-const DashboardCard = ({ title, amount, subtitle, buttonText, icon, toLink }) => {
-  return (
-    <Card className="mt-6 w-96">
-      <CardBody>
-        <div className='flex justify-between items-center'>
-          {icon}
-          <Typography className='text-4xl font-bold text-blush-950'>{amount}</Typography>
-        </div>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          {title}
-        </Typography>
-        <Typography>
-          {subtitle}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Link href={toLink} className="inline-block">
-          <Button size="sm" variant="text" className="flex items-center gap-2 hover:bg-blush-500 hover:text-blush-50">
-            {buttonText}
-            <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-          </Button>
-        </Link>
-      </CardFooter>
-    </Card>
-  )
-}
+export default async function HomePage() {
+  const usersNumber = await numberOfUsers()
+  const patientsNumber = await numberOfPatients()
 
-export default function HomePage() {
   return (
     <Section>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <DashboardCard
+        <CardDashboard
           title="Usuarios"
           subtitle="Gestionar los usuarios del sistema"
           buttonText="Ver"
           icon={<UserIcon strokeWidth={2} className="h-16 w-16 mb-4 text-blush-900" />}
-          amount={588}
+          amount={usersNumber}
           toLink="/users"
         />
-        <DashboardCard
+        <CardDashboard
           title="Pacientes"
           subtitle="Gestionar los pacientes del sistema"
           buttonText="Ver"
           icon={<UserIcon strokeWidth={2} className="h-16 w-16 mb-4 text-blush-900" />}
-          amount={588}
+          amount={patientsNumber}
           toLink="/patients"
         />
       </div>
--- src/app/(pages-not-layout)/unauthorized/page.jsx
+++ src/app/(pages-not-layout)/unauthorized/page.jsx
@@ -11,10 +11,10 @@ import { signOut } from "next-auth/react";
 export default function UnauthorizedPage() {
   return (
     <>
-      <Section className="text-center flex flex-col gap-4 items-center justify-center h-screen">
+      <Section className="text-center flex flex-col gap-4 items-center justify-center h-screen max-w-[800px] mx-auto">
         <h1 className="font-bold text-5xl uppercase">#403</h1>
         <h2 className="font-bold text-3xl">{`Acceso denegado`}</h2>
-        <h4 className="text-xl">{`Comuníquese con un administrador para habilitar el acceso`}</h4>
+        <h4 className="text-xl text-wrap">{`Comuníquese con un administrador del sistema para que le otorgue los permisos necesarios.`}</h4>
         <Image src="/unauthorized.svg" alt="No autorizado" width={300} height={300} />
         <Button
           variant="outlined"
