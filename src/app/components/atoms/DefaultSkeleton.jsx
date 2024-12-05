08c05f4680edb23398a9a620ade3afe9ce9caf1b
Eduardo <ejrb1234@gmail.com>
2024-12-04 20:05:26 -0400

    Updated Skeleton

--- src/app/components/atoms/DefaultSkeleton.jsx
+++ src/app/components/atoms/DefaultSkeleton.jsx
@@ -1,42 +1,34 @@
+import propTypes from 'prop-types'
 import { Typography } from '@/app/MTailwind'
-export function DefaultSkeleton() {
+export function DefaultSkeleton({ className = '', times = 2 }) {
   return (
-    <div className="max-w-full animate-pulse">
-      <Typography
-        as="div"
-        variant="h2"
-        className="mb-4 h-3 w-56 rounded-full bg-gray-300"
-      >
-        &nbsp;
-      </Typography>
-      <Typography
-        as="div"
-        variant="paragraph"
-        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
-      >
-        &nbsp;
-      </Typography>
-      <Typography
-        as="div"
-        variant="paragraph"
-        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
-      >
-        &nbsp;
-      </Typography>
-      <Typography
-        as="div"
-        variant="paragraph"
-        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
-      >
-        &nbsp;
-      </Typography>
-      <Typography
-        as="div"
-        variant="paragraph"
-        className="mb-2 h-2 w-72 rounded-full bg-gray-300"
-      >
-        &nbsp;
-      </Typography>
+    <div className={`max-w-full animate-pulse w-full ${className}`}>
+      <>
+        {Array.from({ length: times }, (_, index) => (
+          <>
+            <Typography
+              key={index}
+              as="div"
+              variant="h2"
+              className={`mb-2 h-3 rounded-full bg-gray-300 ${index % 2 === 0 ? 'w-[85%]' : 'w-1/2'}`}
+            >
+              &nbsp;
+            </Typography>
+            <Typography
+              as="div"
+              variant="paragraph"
+              className="mb-2 h-2 w-full rounded-full bg-gray-300"
+            >
+              &nbsp;
+            </Typography>
+          </>
+        ))}
+      </>
     </div>
   )
+}
+
+DefaultSkeleton.propTypes = {
+  className: propTypes.string,
+  times: propTypes.number,
 }
