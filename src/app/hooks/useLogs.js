2b9f30113cebf21b3ea6349763295dfb57f2968b
Eduardo <ejrb1234@gmail.com>
2024-12-12 13:45:20 -0400

    customHook para los logs

--- src/app/hooks/useLogs.js
+++ src/app/hooks/useLogs.js
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 export function useLogs({ initialStateLogs }) {
   const [
     logs,
     // setLogs
   ] = useState(initialStateLogs)
+  const [filter, setFilter] = useState('')
+  const handleFilterChange = e => {
+    setFilter(e.target.value)
+  }
 
-  return { logs }
+  const filteredLogs = useMemo(() => {
+    return logs.filter(log =>
+      log.correo?.toString().toLowerCase().includes(filter.toLowerCase())
+    )
+  }, [logs, filter])
+
+  return { logs: filteredLogs, filter, handleFilterChange }
 }
